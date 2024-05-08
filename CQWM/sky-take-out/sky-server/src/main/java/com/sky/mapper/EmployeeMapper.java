59af3d2e991b8b590f5787dd9178ac16c5c9b94a
fcawyxb <2377423295@qq.com>
2024-05-08 14:36:29 +0800

    update

--- CQWM/sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ CQWM/sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -13,6 +15,7 @@ public interface EmployeeMapper {
      * @param username
      * @return
      */
+
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 /**
@@ -23,8 +26,19 @@ public interface EmployeeMapper {
  * @description
  * @date 2024/4/29 21:00
  */
+
 @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user,status) " +
         "values " +
         "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
     void insert(Employee employee);
+
+/**
+ * @param employeePageQueryDTO:
+ * @return Page<Employee>
+ * @author yxb
+ * @description 分页查询
+ * @date 2024/5/4 23:12
+ */
+
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
