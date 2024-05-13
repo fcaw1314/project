0115da2b035ce6b782247335e7283275ede55130
fcawyxb <2377423295@qq.com>
2024-05-13 23:35:40 +0800

    美佳肴

--- CQWM/sky-take-out/sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ CQWM/sky-take-out/sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.sky.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Employee implements Serializable {
+public class    Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
