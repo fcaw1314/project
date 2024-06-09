922ee8f87eee459c9c0212d59044796c7bcf6c63
fcawyxb <2377423295@qq.com>
2024-06-09 15:50:54 +0800

    修改菜品业务代码开发

--- CQWM/sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ CQWM/sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
@@ -39,4 +41,12 @@ public interface DishMapper {
      */
     @Delete("delete from dish where id = #{id}")
     void deleteById(Long id);
+
+    /**
+     * 根据id动态修改菜品数据
+     *
+     * @param dish
+     */
+    @AutoFill(value = OperationType.UPDATE)
+    void update(Dish dish);
 }
