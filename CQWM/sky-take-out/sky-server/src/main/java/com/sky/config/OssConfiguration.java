340b9efa2058524c849781e588df3f542c06c724
fcawyxb <2377423295@qq.com>
2024-06-09 12:12:28 +0800

    新增菜品业务代码开发

---
+++ CQWM/sky-take-out/sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,37 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ProjectName: sky-take-out
+ * @Package: com.sky.config
+ * @ClassName: OssConfiguration
+ * @author: Yxb
+ * @Description: 配置类,用于创建AliossUtil对象
+ * @Date: 2024/6/8 21:00
+ * @Version: 1.0
+ */
+
+
+@Configuration
+@Slf4j
+public class OssConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties){
+        log.info("开始创建阿里云文件上传工具类对象:{}",aliOssProperties);
+
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+
+
+    }
+}
