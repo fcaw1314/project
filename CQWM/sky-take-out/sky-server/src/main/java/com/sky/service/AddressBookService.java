6a71496f46b716efa8e11ad67eae4895ba11e078
fcawyxb <2377423295@qq.com>
2024-06-14 09:08:12 +0800

    结束

--- CQWM/sky-take-out/sky-server/src/main/java/com/sky/service/AddressBookService.java
+++ CQWM/sky-take-out/sky-server/src/main/java/com/sky/service/AddressBookService.java
@@ -1,3 +1,23 @@
+//package com.sky.service;
+//
+//import com.sky.entity.AddressBook;
+//import java.util.List;
+//
+//public interface AddressBookService {
+//
+//    List<AddressBook> list(AddressBook addressBook);
+//
+//    void save(AddressBook addressBook);
+//
+//    AddressBook getById(Long id);
+//
+//    void update(AddressBook addressBook);
+//
+//    void setDefault(AddressBook addressBook);
+//
+//    void deleteById(Long id);
+//
+//}
 package com.sky.service;
 
 import com.sky.entity.AddressBook;
@@ -18,3 +38,4 @@ public interface AddressBookService {
     void deleteById(Long id);
 
 }
+
